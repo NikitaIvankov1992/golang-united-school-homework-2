9ee07d83509a28eb13e5ce62aff3f574ffd0ca4b
Nikita Ivankov <nikita.ivankov@share-now.com>
2022-06-05 19:29:30 +0200

    fix(task02-solution): add Pi constant

--- solution.go
+++ solution.go
@@ -7,7 +7,7 @@ import (
 func CalcSquare(sideLen float64, sidesNum int64) float64 {
 	switch sidesNum {
 	case 0:
-		return 3.14 * (sideLen * sideLen)
+		return math.Pi * (sideLen * sideLen)
 	case 3:
 		return 0.5 * sideLen * ((sideLen * math.Sqrt(3)) / 2)
 	case 4:
